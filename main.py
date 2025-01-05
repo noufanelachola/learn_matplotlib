e88de40c05336bbc7feea7c30f4d4b0df9bffffd
noufanelachola <noufansadiqelachola@gmail.com>
2025-01-05 14:11:43 +0530

    basic

---
+++ main.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+X = [1,2,3,4,5]
+Y = [1,2,3,4,5]
+
+plt.plot(X,Y,linestyle='--', marker='o')
+plt.title("Basics")
+plt.xlabel("X-label")
+plt.ylabel("Y-label")
+
+plt.show()
